631652cf0cf8d8975aaad667716e01bbdbafe027
Ben Hunter <learntocode@benhunter.me>
2021-09-30 13:55:04 -0500

    Spring Initializr

---
+++ src/main/java/com/benhunterlearn/springcrudrecipes/SpringCrudRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.benhunterlearn.springcrudrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudRecipesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benhunterlearn/springcrudrecipes/SpringCrudRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benhunterlearn.springcrudrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
